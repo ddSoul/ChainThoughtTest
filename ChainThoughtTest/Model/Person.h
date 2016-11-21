6508580037f57b617a430e573d99eff274879fa2
dddxl <iosmrdeng@163.com>
2016-11-21 15:24:32 +0800

    增加方法说明文字

--- ChainThoughtTest/Model/Person.h
+++ ChainThoughtTest/Model/Person.h
@@ -10,6 +10,13 @@
 
 @interface Person : NSObject
 
+/***
+ 
+ +()方法只是想通过block获取Person类实例对象，
+ - ()方法只是一个get方法，返回值类型为Block,该Block带有返回值Person对象。
+ 
+ ***/
+
 + (void)initPerson:(void (^)(Person *person)) parterm;
 
 - (Person * (^)(NSString *nameString)) KNameString;
